84b574610b1d76b43aafaac3b8a148b7d2b12a63
TDMeyers <meyers.dylan@outlook.com>
2023-07-28 15:07:42 -0600

    testing for deploy phase 2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
     return(
         <div className="nav">
-            <Link to="wondrous-crostata-157ac4.netlify.app/">
+            {/* <Link to="wondrous-crostata-157ac4.netlify.app/">
                 <div>Home</div>
-            </Link>
+            </Link> */}
         </div>
     )
 }
