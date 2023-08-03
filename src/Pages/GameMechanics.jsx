b74ec8c371e7f65d5d95d6f7023c769333b3849c
TDMeyers <meyers.dylan@outlook.com>
2023-08-02 21:17:51 -0600

    Updated damage types

--- src/Pages/GameMechanics.jsx
+++ src/Pages/GameMechanics.jsx
@@ -1,9 +1,11 @@
 import DisplayConditions from "../components/Game Mechanics/DisplayConditions";
+import DisplayDamageTypes from "../components/Game Mechanics/DisplayDamageTypes";
 
 export default function GameMechanics() {
     return (
         <div>
             <DisplayConditions />
+            <DisplayDamageTypes />
         </div>
     )
 }
---
+++ src/components/Game Mechanics/DisplayDamageTypes.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDamageTypesData, fetchDamageTypeData } from "../../Thunks/Game Mechanics/fetchDamageTypesData";
+
+
+const DisplayDamageTypes = () => {
+    const dispatch = useDispatch();
+    const { data, selectedDamageType, loading, error } = useSelector(
+        (state) => state.damageTypes
+    );
+
+    useEffect(() => {
+        dispatch(fetchDamageTypesData());
+    }, [dispatch]);
+
+    const handleFetchWithDamageType = (damageTypeIndex) => {
+        dispatch(fetchDamageTypeData(damageTypeIndex));
+    };
+
+    return (
+        <div>
+            <h2>Damage Types:</h2>
+            {loading ? (
+                <p>Loading damage types...</p>
+            ) : error ? (
+                <p>Error: {error}</p>
+            ) : (
+                <>
+                    {data && data.length > 0 ? (
+                        data.map((damageType) => (
+                            <button
+                                key={damageType.index}
+                                onClick={() => handleFetchWithDamageType(damageType.index)}
+                            >
+                                {damageType.name}
+                            </button>
+                        ))
+                    ) : (
+                        <p>No damage type data available</p>
+                    )}
+
+                    {selectedDamageType && (
+                        <>
+                            <h3>{selectedDamageType.name}</h3>
+                            <p>Description: {selectedDamageType.desc}</p>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default DisplayDamageTypes;
