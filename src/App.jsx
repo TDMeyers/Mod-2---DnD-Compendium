041dafe6cb8e369f78d8e6d8a971e961895e672d
TDMeyers <meyers.dylan@outlook.com>
2023-08-03 16:20:33 -0600

    updated pathing

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route
-          path="/Home"
+          path="/"
           element={<Homepage />}
         />
         <Route
