df0c74bade10b22268a720f80bff858defb3b4af
TDMeyers <meyers.dylan@outlook.com>
2023-08-03 22:28:13 -0600

    testing new path

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -40,7 +40,7 @@ export default function Homepage() {
                             <CardMedia
                                 sx={{ height: '100%', width:'100%' }}
                                 component="img"
-                                image="/src/Styles/DDLogo.png"
+                                image="Styles/DDLogo.png"
                                 title="D & D Logo"
                             />
                             <Typography variant="body1">"Embark on an epic journey through the realms of Dungeons & Dragons with our comprehensive compendium website! Unravel the mysteries of forgotten lore, unearth powerful spells and magical artifacts, and discover a treasure trove of character options. Whether you're a seasoned adventurer or a curious novice, our complete resource collection will ignite your imagination and enhance your gaming experience. Delve into a world of boundless creativity and endless possibilities. Begin your quest now and let the D&D compendium be your ultimate companion in the realms of fantasy."</Typography>
